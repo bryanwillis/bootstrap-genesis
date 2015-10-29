994db75291973eb57a9e170f6838cb3097ce6cbf
Bryan Willis <willis.bryan@live.com>
2015-10-29 04:26:12 -0500

    Update footer-creds.php

--- lib/footer-creds.php
+++ lib/footer-creds.php
@@ -7,3 +7,9 @@ function bsg_footer_creds_filter( $creds ) {
     $creds = "<a {$rel} href=\"http://ironcodestudio.com/bootstrap-genesis-theme/\">Bootstrap Genesis Theme</a> by <a {$rel} href=\"http://ironcodestudio.com/\">Iron Code Studio</a>";
     return $creds;
 }
+
+add_filter( 'genesis_footer_output', 'bsg_footer_creds_filter', 10, 3 );
+function bsg_footer_creds_filter( $creds ) {
+	$creds = '<p class="text-center">[footer_copyright] &middot; <a href="'. esc_url( home_url( '/' ) ) .'">'. get_bloginfo( 'name' ) .'</a></p>';
+	return $creds;
+}
